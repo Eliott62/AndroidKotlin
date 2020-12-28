4c98ec538cc4cd6798c0232760aba225e196c573
Eliott62 <bernardou.eliott62@gmail.com>
2020-12-28 13:47:29 +0100

    Add Unit Test for Usecases

---
+++ app/src/test/java/com/example/androidkotlin/domain/usecase/GetUserUseCaseTest.kt
@@ -0,0 +1,48 @@
+package com.example.androidkotlin.domain.usecase
+
+import com.example.androidkotlin.data.repository.UserRepository
+import com.example.androidkotlin.domain.entity.User
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class GetUserUseCaseTest{
+    private val userRepository: UserRepository = mockk()
+    private val classUnderTest = GetUserUseCase(userRepository)
+
+    @Test
+    fun `invoke with invalid email`(){
+        runBlocking {
+            //GIVEN
+            val email = ""
+            coEvery { userRepository.getUser(email) } returns null
+
+            //WHEN
+            val result = classUnderTest.invoke(email)
+
+            //THEN
+            coVerify (exactly = 1) {userRepository.getUser(email)}
+            assertEquals(result,null)
+        }
+    }
+
+    @Test
+    fun `invoke with valid email`(){
+        runBlocking {
+            //GIVEN
+            val email = "a@b.c"
+            val expectedUser =  User("a@b.c")
+            coEvery { userRepository.getUser(email) } returns expectedUser
+
+            //WHEN
+            val result = classUnderTest.invoke(email)
+
+            //THEN
+            coVerify (exactly = 1) {userRepository.getUser(email)}
+            assertEquals(result,expectedUser)
+        }
+    }
+}
